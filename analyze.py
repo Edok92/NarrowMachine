0ce29c7c2ff90072cb40246b17f3bd83c636ae6a
SelimChehimi <selim.chehimi@efrei.net>
2018-05-24 15:11:01 +0200

    Add files via upload

--- analyze.py
+++ analyze.py
@@ -1,64 +1,71 @@
 # -*- coding: utf-8 -*-
 import csv 
+import connect
 
 Dico = [["mot", 0.0, 0.0, 0.0, 0.0, 0.0, 0]]
 
 # On stocke les valeurs du dictionnaire
-with open('dictionnaire.csv') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        new = [row['mot'], float(row['enthousiaste'])/float(row['appearance']), float(row['content'])/float(row['appearance']), float(row['neutre'])/float(row['appearance']),
-              float(row['triste'])/float(row['appearance']), float(row['colere'])/float(row['appearance']), row['appearance']]
-        Dico.append(new)
+def delete_accent(element):
+        """ supprime les accents des tweets """
+        accents = { 'a': ['à', 'ã', 'á', 'â'],
+                    'e': ['é', 'è', 'ê', 'ë'],
+                    'i': ['î', 'ï'],
+                    'u': ['ù', 'ü', 'û'],
+                    'o': ['ô', 'ö']}
+        for (char, accented_chars) in accents.items():
+            for accented_char in accented_chars:
+                element = element.replace(accented_char, char)
+        return element
+
+def affiche_resultat(phrase):
+    print(phrase)
+    words = phrase.split()
+    size = len(words)
+    result = [0.0, 0.0, 0.0, 0.0, 0.0]
+    conn = connect.connect()
+    nb = 0
+    cur = conn.cursor()
+    for word in words:
+        word = delete_accent(word)
+        cur.execute('select * from dico where mot = %s;',[word])
+        rows = cur.fetchone()
         
-phrase = "Le ciel est triste"
-
-result = [0.0, 0.0, 0.0, 0.0, 0.0]
-# 0 : enthousiaste
-# 1 : content
-# 2 : neutre
-# 3 : triste
-# 4 : colere
-
-words = phrase.split()
-size = len(words)
-for word in words:
-    for i in range(0, len(Dico)):
-        if Dico[i][0] == word:
-            # (enthousiaste + content) - (triste + colere) > 0.2 => on prend le mot en compte
-            # sinon non
-            if abs((Dico[i][1] + Dico[i][2]) - (Dico[i][3] + Dico[i][4])) > 0.2:            
-                result[0] += float(Dico[i][1]) / float(size)    # enthousiaste
-                result[1] += float(Dico[i][2]) / float(size)    # content
-                result[2] += float(Dico[i][3]) / float(size)    # neutre
-                result[3] += float(Dico[i][4]) / float(size)    # triste
-                result[4] += float(Dico[i][5]) / float(size)    # colere
+        if rows is None:
+            print("")
+            return (result, 0)    
+        else:
+            print(word)
+            appearance = rows[6] #appearance
             
-print ""
+            enthousiaste = rows[1] / appearance #'enthousiaste'
+            content = rows[2] / appearance#'content'
+            neutre = rows[3] / appearance#'neutre'
+            triste = rows[4] / appearance#'triste'
+            colere = rows[5] / appearance#colere
+    
+            result[0] += enthousiaste
+            result[1] += content
+            result[2] += neutre
+            result[3] += triste
+            result[4] += colere
+        
+            result[0] /= size
+            result[1] /= size
+            result[2] /= size
+            result[3] /= size
+            result[4] /= size
             
-print "Présence de sentiments dans la phrase : "
-print ""
-print phrase
-print "Enthousiaste : "
-print result[0] * 100, "%" 
-print "Content : " 
-print result[1] * 100, "%"
-print "Neutre : "
-print result[2] * 100, "%"
-print "Triste : "
-print result[3] * 100, "%"
-print "Colere : "
-print result[4] * 100, "%"
-
-print ""
-print "------------Affichage du sentiment général-----------"
-if result[0] == max(result):
-    print "==> Enthousiaste"
-elif result[1] == max(result):
-    print "==> Content"
-elif result[2] == max(result):
-    print "==> Neutre"
-elif result[3] == max(result):
-    print "==> Triste"
-elif result[4] == max(result):
-    print "==> Colere"
+            nb += 1
+            return (result, 1)
+            
+    #if nb > 0 :
+    #    return (result, 1)
+    #else:
+    #    return(result, 0)
+    
+    #result[0] *= 100
+    #result[1] *= 100
+    #result[2] *= 100
+    #result[3] *= 100
+    #result[4] *= 100
+    #result[5] *= 100
